d25ffef04d29696e6e0d69aa49969c6822c61b0d
Dong Hun Cho <johun204@gmail.com>
2020-10-08 12:31:55 +0900

    Update download_Ouluvs2.py

--- Ouluvs2/download_Ouluvs2.py
+++ Ouluvs2/download_Ouluvs2.py
@@ -13,6 +13,9 @@ urls = ["http://www.ee.oulu.fi/research/imag/OuluVS2/OuluVS2-zip/readme",
 "http://www.ee.oulu.fi/research/imag/OuluVS2/OuluVS2-zip/transcript_sentence.zip"]
 
 
+for url in urls:
+	os.system("wget --user " + username + " --password " + password + " " + url)
+
 for sid in range(1, 54):
 	if sid == 29: continue # Video data of Subject 29 turned out to be unusable since his mouth was not seen most of the time.
 	url = "http://www.ee.oulu.fi/research/imag/OuluVS2/OuluVS2-zip/orig_s{}.zip".format(sid)
